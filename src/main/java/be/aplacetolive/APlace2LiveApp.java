fa18c5bba8fc352150324fb80d3984ffed013bf6
rmedard <00Derfine@5>
2017-07-02 20:55:09 +0200

    Kubuntu

--- src/main/java/be/aplacetolive/APlace2LiveApp.java
+++ src/main/java/be/aplacetolive/APlace2LiveApp.java
@@ -2,14 +2,21 @@ package be.aplacetolive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by Medard on 12/05/2017.
  */
 
 @SpringBootApplication
-public class APlace2LiveApp {
+public class APlace2LiveApp extends SpringBootServletInitializer {
     public static void main(String[] args){
         SpringApplication.run(APlace2LiveApp.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(APlace2LiveApp.class);
     }
 }
